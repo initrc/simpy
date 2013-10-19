e91bacacef9a8d71e3bda13df690e0825cd2c290
David Shi <DavidSnooker@gmail.com>
2013-10-19 01:52:44 -0700

    Include csrf_token in template and remove csrf_exempt in view

--- simpyapp/views.py
+++ simpyapp/views.py
@@ -1,11 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return render(request, 'index.html')
 
-@csrf_exempt
 def compare(request):
     doc1 = request.POST.get('doc1')
     doc2 = request.POST.get('doc2')
